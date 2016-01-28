e8abe3cd53e0494de11f44f9d6c819939ace13e6
Tomoya Hirano <tomoya@cookpad.com>
2016-01-28 23:12:58 +0100

    Should support these options

--- lib/gracenote/client.rb
+++ lib/gracenote/client.rb
@@ -46,6 +46,15 @@ module Gracenote
       request[:lang] = options[:lang] == true ? lang : options[:lang] if options[:lang]
       request[:auth] = options[:auth] == true ? auth : options[:auth] if options[:auth]
 
+      # <OPTION>
+      #   <PARAMETER>SELECT_EXTENDED</PARAMETER>
+      #   <VALUE>COVER,REVIEW,ARTIST_BIOGRAPHY,ARTIST_IMAGE,ARTIST_OET,MOOD,TEMPO</VALUE>
+      # </OPTION>
+      # <OPTION>
+      #   <PARAMETER>SELECT_DETAIL</PARAMETER>
+      #   <VALUE>GENRE:3LEVEL,MOOD:2LEVEL,TEMPO:3LEVEL,ARTIST_ORIGIN:4LEVEL,ARTIST_ERA:2LEVEL,ARTIST_TYPE:2LEVEL</VALUE>
+      # </OPTION>
+
       { queries: request.merge(query: params.merge(:@cmd => cmd)) }
     end
   end
